8788812401a3722761a16a3430421b451bb5c456
Amit <81560269+immeas@users.noreply.github.com>
2025-02-06 11:58:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+    let spaceIndex = s.indexOf(' ');
+
+    // If no space is found, return the whole string
+    return spaceIndex === -1 ? str : str.substring(0, spaceIndex);
+}
 }
 
-// Do not change the code below
+Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
