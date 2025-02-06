1841ec34c4e6039c355677515626f7aa5664afeb
Amit <81560269+immeas@users.noreply.github.com>
2025-02-06 12:20:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function firstWord(s) {
-  // your code here
-	str = s.trim();
-    let spaceIndex = str.indexOf(' ');
+    // Trim leading and trailing spaces
+    s = s.trim();
+    let spaceIndex = s.indexOf(' ');
 
     // If no space is found, return the whole string
-    return spaceIndex
+    return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
 }
 }
 
